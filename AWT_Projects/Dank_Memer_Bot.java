2f80a759067026b04959c0c2be5e6879d28dde7b
RohanCheri <34838212+RohanCheri@users.noreply.github.com>
2021-02-10 15:30:30 -0500

    Added program to automatically conduct commands in dank memer bot

--- AWT_Projects/Dank_Memer_Bot.java
+++ AWT_Projects/Dank_Memer_Bot.java
@@ -2,16 +2,16 @@ package JavaProjects.AWT_Projects;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
-import java.awt.event.MouseAdapter;
+import java.util.*;
+import java.io.*;
 
 /**
- * This program takes any string and types it out
- * a specified amount of times.
+ * This program outputs command for the dank memer bot
  *
  * @author Rohan
- * @version 9/26/2020
+ * @version 2/10/2021
  */
-public class SendText {
+public class Dank_Memer_Bot {
     // Variables that index letters and symbols
     public static String alphabet = "abcdefghijklmnopqrstuvwxyz";
     public static String symbols = ",-./0123456789;=";
@@ -29,10 +29,9 @@ public class SendText {
     public static int waitTime = 45000;
 
     // Text to send
-    public static String sendString = "pls beg";
-
-    // How many times the text repeats (Use 0 for infinite)
-    public static int repeats = 5;
+    public static String[] commands = {"pls_beg", "pls_highlow L", "pls_fish", "pls_postmemes c"};
+    public static int[] times = {46000, 21500, 61000, 61000};
+    public static int[] cTimes = {0, 0, 0, 0};
 
     public static void main(String[] args) throws AWTException, InterruptedException {
         // Instantiates robot object
@@ -41,20 +40,40 @@ public class SendText {
         // Runs startup delay
         Thread.sleep(startDelay);
 
+        long lTime = System.currentTimeMillis(), cTime, aTime;
         // Main repeat loop
-        while(repeats --> 0){
-            // Type letter by letter
-            for(int i = 0; i < sendString.length(); i++){
-                type(sendString.substring(i, i + 1));
+        while(true){
+            cTime = System.currentTimeMillis();
+            aTime = cTime - lTime;
+            lTime = cTime;
+
+            for(int i = 0; i < cTimes.length; i++){
+                cTimes[i] += aTime;
+
+                if(cTimes[i] > times[i]){
+                    cTimes[i] = 0;
+                    String[] cmds = commands[i].split(" ");
+
+                    for(String c : cmds){
+                        typeText(c);
+                        Thread.sleep(1000);
+                    }
+                }
             }
-
-            // Hits enter to send text
-            click(KeyEvent.VK_ENTER);
-
-            // Wait for idle processes to complete
-            robot.waitForIdle();
-            Thread.sleep(waitTime);
         }
+    }
+
+    public static void typeText(String str){
+        // Type letter by letter
+        for(int i = 0; i < str.length(); i++){
+            type(str.substring(i, i + 1));
+        }
+
+        // Hits enter to send text
+        click(KeyEvent.VK_ENTER);
+
+        // Wait for idle processes to complete
+        robot.waitForIdle();
     }
 
     // Takes in a letter and types it out to the robot
@@ -69,7 +88,7 @@ public class SendText {
         }
 
         // Check for specific symbols
-        if(letter.equals(" ")){
+        if(letter.equals(" ") || letter.equals("_")){
             click(KeyEvent.VK_SPACE);
             robot.waitForIdle();
         }
